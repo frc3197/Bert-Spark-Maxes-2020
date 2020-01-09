ada90eab0438d52ae0fa4171ee38d3052471a4b3
Pineapple42 <frozenfire71@gmail.com>
2020-01-08 19:13:13 -0600

    Revert "Drivetrain completed for the most post, need to fix instances in Robot"
    This reverts commit f016d1b6b73d0c792d4433a17fe49a6b0628937a.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,15 +7,18 @@
 
 package frc.robot;
 
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants. This class should not be used for any other
- * purpose. All constants should be declared globally (i.e. public static). Do
- * not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants.  This class should not be used for any other purpose.  All constants should be
+ * declared globally (i.e. public static).  Do not put anything functional in this class.
  *
- * <p>
- * It is advised to statically import this class (or one of its inner classes)
- * wherever the constants are needed, to reduce verbosity.
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
  */
 public final class Constants {
     public static enum CANSparkMaxID {
@@ -28,5 +31,4 @@ public final class Constants {
           this.id = id;
           this.name = name;
         }
-}
 }
