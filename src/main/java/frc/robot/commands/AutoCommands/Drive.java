7d8fe0fb12605a734b4ed19c98d2639a68f511a2
YuriKleyman <40747797+YuriKleyman@users.noreply.github.com>
2020-01-09 19:29:30 -0600

    Update Drive.java

--- src/main/java/frc/robot/commands/AutoCommands/Drive.java
+++ src/main/java/frc/robot/commands/AutoCommands/Drive.java
@@ -1,5 +1,6 @@
 package frc.robot.commands.AutoCommands;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.DriveTrain;
@@ -32,6 +33,8 @@ public class Drive extends CommandBase {
     double tankR = RobotContainer.tankDriveRight();
     double l = RobotContainer.tankDriveLeft();
     driveTrain.tankDrive(tankR, l);
+    SmartDashboard.putNumber("Left Motor Input", l);
+    SmartDashboard.putNumber("Right Motor Input", tankR);
   }
 
 
