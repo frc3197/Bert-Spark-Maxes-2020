1b8b0e3da84e970c4d332ae37fb613384218c324
Pineapple42 <32251990+Jordan-Limonov@users.noreply.github.com>
2020-01-09 18:39:35 -0600

    Updated from CANSparkMax to Talon SRX's
    CANSparkMax implementation still exists, it's just commented out in case we still use it.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,17 +3,15 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj.XboxController;
 
  public class RobotContainer {
-
-
-
   private static XboxController driver = new XboxController(0);
+  private final ExampleSubsystem m_autoSubsystem = new ExampleSubsystem();
+  private final ExampleCommand m_autoCommand = new ExampleCommand(m_autoSubsystem);
 
-
- 
- 
  /*
    private void configureButtonBindings() {
 
--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -1,22 +1,27 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.CANSparkMax.IdleMode;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 public class DriveTrain extends SubsystemBase {
-  private CANSparkMax l1SparkMax = new CANSparkMax(Constants.CANSparkMaxID.kLeft1.id, MotorType.kBrushless);
-  private CANSparkMax r1SparkMax = new CANSparkMax(Constants.CANSparkMaxID.kRight1.id, MotorType.kBrushless);
-  private CANSparkMax r2SparkMax = new CANSparkMax(Constants.CANSparkMaxID.kRight2.id, MotorType.kBrushless);
-  private CANSparkMax l2SparkMax = new CANSparkMax(Constants.CANSparkMaxID.kLeft2.id, MotorType.kBrushless);
+  private Talon l1Talon = new Talon(Constants.TalonID.kLeft1.id);
+  private Talon l2Talon = new Talon(Constants.TalonID.kLeft2.id);
+  private Talon r1Talon = new Talon(Constants.TalonID.kRight1.id);
+  private Talon r2Talon = new Talon(Constants.TalonID.kRight2.id);
+  // private CANSparkMax l1SparkMax = new CANSparkMax(Constants.CANSparkMaxID.kLeft1.id, MotorType.kBrushless);
+  // private CANSparkMax r1SparkMax = new CANSparkMax(Constants.CANSparkMaxID.kRight1.id, MotorType.kBrushless);
+  // private CANSparkMax r2SparkMax = new CANSparkMax(Constants.CANSparkMaxID.kRight2.id, MotorType.kBrushless);
+  // private CANSparkMax l2SparkMax = new CANSparkMax(Constants.CANSparkMaxID.kLeft2.id, MotorType.kBrushless);
 
-  private SpeedControllerGroup leftMotors = new SpeedControllerGroup(l1SparkMax, l2SparkMax);
-  private SpeedControllerGroup rightMotors = new SpeedControllerGroup(r1SparkMax, r2SparkMax);
+  private SpeedControllerGroup leftMotors = new SpeedControllerGroup(l1Talon, l2Talon);
+  private SpeedControllerGroup rightMotors = new SpeedControllerGroup(r1Talon, r2Talon);
 
   private DifferentialDrive drive = new DifferentialDrive(leftMotors, rightMotors);
 
@@ -26,10 +31,10 @@ public class DriveTrain extends SubsystemBase {
    * Creates a new DriveTrain.
    */
   public DriveTrain() {
-    l1SparkMax.setIdleMode(IdleMode.kBrake);
-    l2SparkMax.setIdleMode(IdleMode.kBrake);
-    r1SparkMax.setIdleMode(IdleMode.kBrake);
-    r2SparkMax.setIdleMode(IdleMode.kBrake);
+    // l1SparkMax.setIdleMode(IdleMode.kBrake);
+    // l2SparkMax.setIdleMode(IdleMode.kBrake);
+    // r1SparkMax.setIdleMode(IdleMode.kBrake);
+    // r2SparkMax.setIdleMode(IdleMode.kBrake);
     }
   
 
