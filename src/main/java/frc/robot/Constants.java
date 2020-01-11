5620347f032b2b805a7b89bc917ccc6b97a181c8
HexHounds <38847793+hexhounds@users.noreply.github.com>
2020-01-10 18:42:36 -0600

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,24 +9,26 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  
+
   public static enum CANSparkMaxID {
-  kLeft1(0, "Left1"), kLeft2(1, "Left2"), kRight1(2, "Right1"), kRight2(3, "Right2");
+    kLeft1(14, "Left1"), kLeft2(15, "Left2"), kRight1(1, "Right1"), kRight2(16, "Right2");
 
     public final int id;
     public final String name;
 
-   private CANSparkMaxID(int id, String name) {
-    this.id = id;
-    this.name = name;
-     }
-   }
+    private CANSparkMaxID(int id, String name) {
+      this.id = id;
+      this.name = name;
+    }
+  }
 }
