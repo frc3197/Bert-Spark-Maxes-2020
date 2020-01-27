2d4190f7de6979c6039c154eb5fc95edd8b80b42
YuriKleyman <40747797+YuriKleyman@users.noreply.github.com>
2020-01-27 07:42:13 -0600

    Hood stuff

---
+++ src/main/java/frc/robot/commands/LimelightHood.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.subsystems.Hood;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class LimelightHood extends PIDCommand {
+  Hood hood;
+
+  /**
+   * Creates a new LimelightHood.
+   */
+  public LimelightHood(Hood hood) {
+    super(
+        // The controller that the command will use
+        new PIDController(0, 0, 0),
+        // This should return the measurement
+        hood::getYOffset,
+        // This should return the setpoint (can also be a constant)
+        0,
+        // This uses the output
+        output -> {
+          hood.moveHood(output * .3);
+        });
+          // Use the output here
+        
+    // Use addRequirements() here to declare subsystem dependencies.
+    // Configure additional PID options by calling `getController` here.
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
