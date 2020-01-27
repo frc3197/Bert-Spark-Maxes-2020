3bee6edb89d54edcd449298b23acd7c45de32292
YuriKleyman <40747797+YuriKleyman@users.noreply.github.com>
2020-01-27 07:32:09 -0600

    Update TrackLimelightFollow.java

--- src/main/java/frc/robot/commands/AutoCommands/TrackLimelightFollow.java
+++ src/main/java/frc/robot/commands/AutoCommands/TrackLimelightFollow.java
@@ -42,6 +42,7 @@ public class TrackLimelightFollow extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    driveTrain.tankDrive(0, 0);
   }
 
   // Returns true when the command should end.
